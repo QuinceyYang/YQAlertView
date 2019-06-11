5356b766d4912e6b4abe386fba87a764cd26a1a0
QuinceyYang <yqing555@qq.com>
2019-06-11 16:19:18 +0800

    Initial Commit

---
+++ YQAlertView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YQAlertView
+//
+//  Created by 杨清 on 2019/6/11.
+//  Copyright © 2019 QuinceyYang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ YQAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YQAlertView
+//
+//  Created by 杨清 on 2019/6/11.
+//  Copyright © 2019 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
